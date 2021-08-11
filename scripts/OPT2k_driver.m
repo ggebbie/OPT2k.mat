c2285a2e934dafa366ce3e15c39f33a48ebf4b37
G Jake Gebbie <ggebbie@whoi.edu>
2021-08-10 22:04:13 -0400

    code complete through inversion, not tested
    many diagnostics, figures not included

--- scripts/OPT2k_driver.m
+++ scripts/OPT2k_driver.m
@@ -11,10 +11,23 @@ challenger_obs
 % set up E observational matrices for basinwide temperature obs
 Ebasins
 
+% set up other necessary and worthwhile E matrices
+Ediags
+
 %% Get appropriate green functions.
 % Use 2 x 2 degree circulation model.
 
 % obtain 2x2 degree model input from TMI repository
 setup_TMI_2x2
 
-get_green_functions_regions
+green_functions_regions
+
+master_greens_functions
+
+setup_basinwide_greens_functions
+
+SST_first_guess
+
+inversion
+
+% inversion_diags; not checked in yet
